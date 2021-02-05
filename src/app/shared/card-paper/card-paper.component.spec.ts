9d2dd3f7bd1bf0a1890479004826faf642f64eeb
Carlos Serrada <carlos.serrada@kavak.com>
2021-02-05 12:22:26 -0600

    Update angular to version 11

--- src/app/shared/card-paper/card-paper.component.spec.ts
+++ src/app/shared/card-paper/card-paper.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CardPaperComponent } from './card-paper.component';
 
@@ -6,7 +6,7 @@ describe('CardPaperComponent', () => {
   let component: CardPaperComponent;
   let fixture: ComponentFixture<CardPaperComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CardPaperComponent ]
     })
